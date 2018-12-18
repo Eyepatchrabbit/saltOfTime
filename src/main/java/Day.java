15e5de34f94224b362a93b8515f7feb058f71c51
alexanderboffin <boffin.alexander@gmail.com>
2018-12-18 17:44:49 +0100

    setting folder structure

---
+++ src/main/java/Day.java
@@ -0,0 +1,32 @@
+import java.time.LocalDate;
+
+public class Day {
+
+    private LocalDate date;
+
+    private String weekName;
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setWeekName(String weekName) {
+        this.weekName = weekName;
+    }
+
+    public String getWeekName() {
+        return weekName;
+    }
+
+
+
+
+
+
+
+}
---
+++ src/test/java/testDayNameSetter.java
@@ -0,0 +1,3 @@
+public class testDayNameSetter {
+    //for first test
+}
