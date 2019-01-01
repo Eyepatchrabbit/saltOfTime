04cf39476e5770911e31b090ed60791bc001ab1d
alexanderboffin <boffin.alexander@gmail.com>
2019-01-01 15:07:02 +0100

    PeriodSetup07: commandline setup
    -make basis of branch to create a period and report from the commandline

---
+++ src/main/java/executionmethods/commandlinehandeling/mainExecutionCommandline.java
@@ -0,0 +1,15 @@
+package executionmethods.commandlinehandeling;
+
+public class mainExecutionCommandline {
+
+    public static void main(String[] args) {
+        //execution
+
+        //ask if want to read info from file or set using the commandline
+
+        //read from file
+
+        //execute class to handle input from the commandline
+
+    }
+}
